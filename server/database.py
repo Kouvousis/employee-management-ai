d3ce382877b5e6fc59e35705cad2a6ecd7d834a1
Kouvousis <dimitris.kouvousis@gmail.com>
2026-06-01 19:23:13 +0300

    feat: add database connection and creation

---
+++ server/database.py
@@ -0,0 +1,32 @@
+import os
+from dotenv import load_dotenv
+from sqlalchemy.exc import SQLAlchemyError
+from sqlmodel import Session, SQLModel, create_engine
+
+load_dotenv()
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+if not DATABASE_URL:
+    raise ValueError("DATABASE_URL environment variable not set")
+
+engine = create_engine(
+    url=DATABASE_URL,
+    echo=os.getenv("DEVELOPMENT_MODE", "false").lower() == "true",
+)
+
+def create_db_and_tables():
+    try:
+        SQLModel.metadata.create_all(engine)
+    except Exception as e:
+        print(f"Error creating database and tables: {e}")
+        raise
+
+
+def get_session():
+    with Session(engine) as session:
+        try:
+            yield session
+        except SQLAlchemyError as e:
+            session.rollback()
+            print(f"Session error: {e}")
+            raise
