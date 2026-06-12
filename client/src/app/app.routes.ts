5d923ede6a3b5fa9d3aaabbe6ba5447baeec53ec
Kouvousis <dimitris.kouvousis@gmail.com>
2026-06-12 21:16:46 +0300

    refactor: update route imports to match pages directory restructuring

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -5,30 +5,30 @@ import { authGuard, hrOrAdminGuard } from './core/auth/auth.guard';
 export const routes: Routes = [
   {
     path: 'login',
-    loadComponent: () => import('./features/auth/login/login').then((m) => m.Login),
+    loadComponent: () => import('./pages/auth/login/login').then((m) => m.Login),
   },
   {
     path: '',
-    loadComponent: () => import('./features/shell/shell').then((m) => m.Shell),
+    loadComponent: () => import('./pages/side-menu/side-menu.component').then((m) => m.SideMenu),
     canActivate: [authGuard],
     children: [
       { path: '', redirectTo: 'employees', pathMatch: 'full' },
       {
         path: 'employees',
         canActivate: [hrOrAdminGuard],
-        loadComponent: () => import('./features/employees/employees').then((m) => m.Employees),
+        loadComponent: () => import('./pages/employees/employees').then((m) => m.Employees),
       },
       {
         path: 'projects',
-        loadComponent: () => import('./features/projects/projects').then((m) => m.Projects),
+        loadComponent: () => import('./pages/projects/projects').then((m) => m.Projects),
       },
       {
         path: 'tasks',
-        loadComponent: () => import('./features/tasks/tasks').then((m) => m.Tasks),
+        loadComponent: () => import('./pages/tasks/tasks').then((m) => m.Tasks),
       },
       {
         path: 'chat',
-        loadComponent: () => import('./features/chat/chat').then((m) => m.Chat),
+        loadComponent: () => import('./pages/chat/chat').then((m) => m.Chat),
       },
     ],
   },
