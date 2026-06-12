7909dffb7814432a48a387403998d9f90c606e2c
Kouvousis <dimitris.kouvousis@gmail.com>
2026-06-12 20:47:55 +0300

    feat: create index file for core models

---
+++ client/src/app/core/models/index.ts
@@ -0,0 +1,6 @@
+export * from './page.model';
+export * from './user.model';
+export * from './employee.model';
+export * from './project.model';
+export * from './task.model';
+export * from './chat.model';
